c29dce2582b7d14edfca00651ae22050b725bb7f
Elliot <78340alex@gmail.com>
2020-10-20 17:29:18 +0200

    dossier recettes

--- App.js
+++ App.js
@@ -4,7 +4,11 @@ import { Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { NavigationContainer, StackActions } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import Home from './screen/Home'
+import TourteauLinguine from './screen/Recipes/TourteauLinguine'
 import Products from './screen/Products'
+import { ReactReduxContext } from 'react-redux';
+/*import (Provider) from 'react-redux'
+import store from "./store/configurestore.js"*/
 
 const Stack = createStackNavigator();
 
@@ -12,6 +16,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false}}>
+        <Stack.Screen name="TourteauLinguine" component={ TourteauLinguine } />
         <Stack.Screen name="home" component={ Home } />
         <Stack.Screen name="products" component={ Products } />
 
