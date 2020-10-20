7228467701e23c315cac0035622a2f0838a9b4f0
gnouf1 <jeuxforum26@gmail.com>
2020-10-20 18:02:50 +0200

    vues bateaux :+1:

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+// import { Provider } from 'react-redux';
+// import store from './store/configureStore';
 import { Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { NavigationContainer, StackActions } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import Home from './screen/Home'
-import Products from './screen/Products'
+// import Products from './screen/Products'
+import batDeLaBrise from './screen/bateaux/deLaBrise'
+// import productList from './screen/productsList'
+// import Cart from './screen/cart';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +17,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false}}>
+      <Stack.Screen name="De La Brise" component={ batDeLaBrise } />
         <Stack.Screen name="home" component={ Home } />
         <Stack.Screen name="products" component={ Products } />
 
@@ -19,5 +25,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-
