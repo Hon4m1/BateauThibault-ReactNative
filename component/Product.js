5d5c764decc05c3acae1339963df42a365010f8e
ratchet9364 <axel.bouvier@outlook.fr>
2020-10-22 16:48:44 +0200

    productList + panier

--- component/Product.js
+++ component/Product.js
@@ -1,5 +1,7 @@
 import { StyleSheet, Text, View, StatusBar, ImageBackground, Image, TouchableOpacity } from 'react-native';
 import { images } from '../constants/Images'
+import Button from '../component/Button';
+
 import React from 'react';
 
 import { addProduct, removeProduct } from '../store/actions/cartActions';
@@ -9,14 +11,14 @@ class Product extends React.Component{
 
     isInCart(){
         let result= this.props.products.filter(value=>{
-            return value.name==this.props.item.name;
+            return value.id==this.props.item.id;
         })
         return result.length>0;
     }
 
 
     render(){
-    return <TouchableOpacity style={StyleSheet.container}
+    return <TouchableOpacity style={styles.container}
     onPress={() =>{
         if(!this.isInCart()){
             this.props.addProduct(this.props.item);
@@ -24,10 +26,11 @@ class Product extends React.Component{
         }else{
             this.props.removeProduct(this.props.item);
         }
+        console.log("bonjour");
     }}>
-        <Image source={images.poulpe.url}></Image>
-        <Text style={{marginVertical:10, textAlign:"center"}}>{this.props.item.name}</Text>
-        <Text>
+        <Image source={require("../assets/poulpe.png")} style={styles.image}></Image>
+        <Text style={styles.productName} >{this.props.item.name}</Text>
+        <Text style={styles.price}>
             {this.isInCart()&& "OK "}
             {this.props.item.price}
         </Text>
@@ -49,3 +52,32 @@ const mapDispatchToProps = dispatch => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Product);
+
+const styles = StyleSheet.create({
+    container: {
+        height: 50,
+        backgroundColor: 'rgba(52, 52, 52, 0.3)',
+        flexDirection: "row",
+        borderTopWidth: 0.2,
+    },
+    image: {
+        height: 40,
+        width: 40,
+        alignSelf: "center",
+        marginLeft: 5,
+    },
+    productName: {
+        fontSize: 17,
+        color: "white",
+        alignSelf: "center",
+        marginLeft: 10,
+    },
+    price: {
+      flex: 1,
+      color: "white",
+      marginLeft: 20,
+      fontSize: 17,
+      alignSelf: "center",
+      flexDirection: "row",
+    }
+})
